ba2c9ec2bfc6270594b03e0e1a64c8257d79e470
emzi16 <emilianozapata0638@gmail.com>
2023-11-27 15:16:18 -0300

    Navbar y Home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,34 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import NavbarStyle from "../styles/components/NavBar.module.css";
 
-function NavBar() {
+function NavBar({ setHovered }) {
+  const onMouseEnter = () => {
+    setHovered(true);
+  };
+  const onMouseLeave = () => {
+    setHovered(false);
+  };
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="#home">Juice Shop</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Products</Nav.Link>
-            <Nav.Link href="#link">About Us</Nav.Link>
-            <Nav.Link href="#link">
-              <i className="bi bi-cart2"></i>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <Nav.Link>All products</Nav.Link>
+        <div
+          className={NavbarStyle.subnav}
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
+        >
+          <button className={NavbarStyle.subnavbtn}>Categories</button>
+          <div className={NavbarStyle.subnavContent}>
+            <Nav.Link>Juices</Nav.Link>
+            <Nav.Link>Elixirs</Nav.Link>
+            <Nav.Link>Nut milks &amp; smoothies</Nav.Link>
+            <Nav.Link>Sparkling tonics</Nav.Link>
+          </div>
+        </div>
       </Container>
     </Navbar>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
+import Glide from "@glidejs/glide";
 
 function Home() {
   const [products, setProducts] = useState(null);
+  const [hovered, setHovered] = useState(false);
 
   const getProducts = async () => {
     axios({
@@ -14,17 +16,28 @@ function Home() {
 
   useEffect(() => {
     getProducts();
+    new Glide(".glide").mount();
   }, []);
-
   return (
     products && (
       <>
-        <Navbar />
-        <div>Home</div>
-        <div>
-          {products.map((product) => (
-            <div key={product._id}>{product._id}</div>
-          ))}
+        <Navbar setHovered={setHovered} />
+        <div style={hovered ? { backdropFilter: "brightness(50%)" } : {}}>
+          <div>Home</div>
+          <div>
+            {products.map((product) => (
+              <div key={product._id}>{product._id}</div>
+            ))}
+          </div>
+          <div className="glide">
+            <div className="glide__track" data-glide-el="track">
+              <ul className="glide__slides">
+                <li className="glide__slide">0</li>
+                <li className="glide__slide">1</li>
+                <li className="glide__slide">2</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </>
     )
