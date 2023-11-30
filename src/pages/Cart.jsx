a2f58c68c54fa0647b8eb923b177d9424cdd7756
FrancoSassi03 <francosassi03@gmail.com>
2023-11-30 15:55:39 -0300

    cart
    update

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -21,70 +21,64 @@ function Cart() {
     <>
       <Navbar setHovered={setHovered} />
       <div className={hovered ? "bg-dark-hover-nav " : ""}>
-      <div className="container">
-        <h1 className="fw-bold">SHOPPING CART</h1>
-        <Table>
-          <thead>
-            <tr>
-              <th className="fw-bold" colSpan={2}>
-                PRODUCTS
-              </th>
-              <th className="fw-bold">PRICE</th>
-              <th className="fw-bold">QUANTITY</th>
-              <th className="fw-bold">TOTAL</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td colSpan={2}>
-                <div className="row">
-                  <div className="col">
-                    <img src="" alt="" />
-                  </div>
-                  <div className="col">
+        <div className="container">
+          <h1 className="woolwich text-center mt-5 mb-5">My basket</h1>
+          <Table>
+            <thead>
+              <tr>
+                <th className="fw-bold" colSpan={2}>
+                  Products
+                </th>
+                <th className="fw-bold">Price</th>
+                <th className="fw-bold">Quantity</th>
+                <th className="fw-bold">Total</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="w-25" colSpan={2}>
+                  <div className="d-flex gap-5" >
+                    <img className="w-25" src="juices.PNG" alt="" />
                     <p>Fall Punch</p>
                   </div>
-                </div>
-              </td>
-              <td>13.00</td>
-              <td>
-                <div>
-                  <button
-                    className="button-size"
-                    onClick={decreaseValue}
-                    disabled={value === 1}
-                  >
-                    -
-                  </button>
-                  <input
-                    className="text-center input-size "
-                    type="number"
-                    value={value}
-                    readOnly
-                  />
-                  <button className="button-size" onClick={increaseValue}>
-                    +
-                  </button>
-                </div>
-              </td>
-              <td>13.00</td>
-              <td>
-                <Link className="text-decoration-none text-danger" to={""}>
-                  <i className="bi bi-x-circle-fill"></i>
-                </Link>
-              </td>
-            </tr>
-            <tr>
-              <td colSpan={3}></td>
-              <td>Subtotal</td>
-              <td className="fw-bold">13.00</td>
-              <td></td>
-            </tr>
-          </tbody>
-        </Table>
-      </div>
-      <Footer />
+                </td>
+                <td>13.00</td>
+                <td>
+                  <div>
+                    <button
+                      className="button-size"
+                      onClick={decreaseValue}
+                      disabled={value === 1}
+                    >
+                      -
+                    </button>
+                    <input
+                      className="text-center input-size "
+                      type="number"
+                      value={value}
+                      readOnly
+                    />
+                    <button className="button-size" onClick={increaseValue}>
+                      +
+                    </button>
+                  </div>
+                </td>
+                <td>13.00</td>
+                <td>
+                  <Link className="text-decoration-none text-danger" to={""}>
+                    <i className="bi bi-x-circle-fill"></i>
+                  </Link>
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+          <div className="d-flex flex-row justify-content-end">
+            <p className="fw-bold me-5">Subtotal</p>
+            <p className="fw-bold">13.00</p>
+          </div>
+        </div>
+        <Footer />
       </div>
     </>
   );
