ec40d1ccd19a253109debc5f894d440f82b55f1c
FrancoSassi03 <francosassi03@gmail.com>
2023-12-06 11:49:19 -0300

    update cart
    ....

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -10,8 +10,9 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 import AddToCart from "../components/AddToCart";
+import CancelIcon from "@mui/icons-material/Cancel";
+import IconButton from "@mui/material/IconButton";
 
 function Cart({ hovered, setShowNavAndFooter }) {
   const dispatch = useDispatch();
@@ -35,82 +36,79 @@ function Cart({ hovered, setShowNavAndFooter }) {
       <div className={hovered ? "bg-dark-hover-nav " : ""}>
         <div className="container ">
           <h1 className="woolwich text-center mt-5 mb-5">My basket</h1>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 700 }} aria-label="spanning table">
-              <TableHead>
-                <TableRow>
-                  <TableCell align="">Products</TableCell>
-                  <TableCell align="">Price</TableCell>
-                  <TableCell align="">Quantity</TableCell>
-                  <TableCell align="">Total</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {cart.items.map((product) => (
-                  <TableRow key={product.item._id}>
-                    <TableCell align="">
-                      <div className="d-flex gap-5 align-items-center">
-                        <img
-                          className="cartImg"
-                          src={product.item.image}
-                          alt=""
-                        />
-                        <p>{product.item.name}</p>
-                      </div>
-                    </TableCell>
-                    <TableCell align="">
-                      <div className="d-flex align-items-center heightCart">
-                        <p>${product.item.price}</p>
-                      </div>
-                    </TableCell>
-                    <TableCell align="">
-                      <div className="d-flex align-items-center heightCart">
-                        {/* <div className="input-group ">
-                          <button
-                            className="btn btn-success"
-                            onClick={decreaseValue}
-                          >
-                            <i className="bi bi-dash text-white"></i>
-                          </button>
-                          <input
-                            className="text-center inputCart "
-                            type="text"
-                            value={product.qty}
-                          />
-                          <button
-                            className="btn btn-success"
-                            onClick={increaseValue}
-                          >
-                            <i className="bi bi-plus-lg text-white"></i>
-                          </button>
-                        </div> */}
-                        <AddToCart product={product.item} qty={product.qty} classBtn={"custom-btn-swiper custom-btn-swiper-product"} typeQty={true} />
-                      </div>
-                    </TableCell>
-                    <TableCell>
-                      <div className="d-flex align-items-center heightCart">
-                      <div className="w-25">
-                        <p className="d-inline ">
-                          {Number(product.qty) * Number(product.item.price)}
-                        </p></div>
-                        <Link
-                          className="text-decoration-none text-danger ms-5"
-                          to={""}
-                        >
-                          <i className="bi bi-x-circle-fill"></i>
-                        </Link>
-                      </div>
-                    </TableCell>
+          {cart.items[0] ? (
+            <TableContainer >
+              <Table sx={{ minWidth: 700 }} aria-label="spanning table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Products</TableCell>
+                    <TableCell>Price</TableCell>
+                    <TableCell>Quantity</TableCell>
+                    <TableCell>Total</TableCell>
                   </TableRow>
-                ))}
-                <TableRow>
-                  <TableCell rowSpan={3} />
-                  <TableCell colSpan={2}>Subtotal</TableCell>
-                  <TableCell align="right">${cart.subtotal}</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
+                </TableHead>
+                <TableBody>
+                  {cart.items.map((product) => (
+                    <TableRow key={product.item._id}>
+                      <TableCell >
+                        <div className="d-flex gap-5 align-items-center">
+                          <img
+                            className="cartImg"
+                            src={product.item.image}
+                            alt=""
+                          />
+                          <p>{product.item.name}</p>
+                        </div>
+                      </TableCell>
+                      <TableCell >
+                        <div className="d-flex align-items-center heightCart">
+                          <p>${product.item.price}</p>
+                        </div>
+                      </TableCell>
+                      <TableCell >
+                        <div className="d-flex align-items-center heightCart">
+                          <AddToCart
+                            product={product.item}
+                            qty={product.qty}
+                            classBtn={
+                              "custom-btn-swiper custom-btn-swiper-product"
+                            }
+                            typeQty={true}
+                          />
+                        </div>
+                      </TableCell>
+                      <TableCell>
+                        <div className="d-flex align-items-center heightCart">
+                          <div className="w-25">
+                            <p className="d-inline ">
+                              {Number(product.qty) * Number(product.item.price)}
+                            </p>
+                          </div>
+                          <IconButton onClick={()=>dispatch(removeOfCart(product))} className="text-danger">
+                    <CancelIcon />
+                  </IconButton>
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                  <TableRow>
+                    <TableCell rowSpan={3} />
+                    <TableCell colSpan={2}>Subtotal</TableCell>
+                    <TableCell align="right">${cart.subtotal}</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+          ) : (
+            <div>
+            <p className="text-center mt-5">YOUR BASKET IS EMPTY.</p>
+            <div className="d-flex justify-content-center w-100">
+            <button className="woolwich btn btn-dark fs-3 btnCheckout m-0 w-25 mt-5">
+              continue shopping
+            </button>
+            </div>
+            </div>
+          )}
         </div>
         <Footer />
       </div>
