384b475631a68258ad54b08ee04d0ac0c482d63c
MatiasBarreto1595 <matias.barreto98@hotmail.com>
2023-12-08 17:08:36 -0300

    success

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import Footer from "../components/Footer";
+import { useEffect } from "react";
 import { useNavigate } from "react-router";
 import SuccessStyle from "../styles/views/Success.module.css";
-function Success() {
+function Success({ setShowNavAndFooter }) {
   const navigate = useNavigate();
+  useEffect(() => {
+    setShowNavAndFooter(true);
+  }, []);
   return (
     <>
-      <div className={` text-center mt-5`}>
+      <video muted autoPlay loop>
+        <source src="./videos/successVideo.mp4" type="video/mp4" />
+      </video>
+
+      <div className={` text-center mt-5 ${SuccessStyle.promo}`}>
         <h1 className={SuccessStyle.SuccessTittle}>Sweet!</h1>
         <p className={SuccessStyle.SuccessText}>
           Your order has been approved.
@@ -23,9 +30,6 @@ function Success() {
         >
           Go to Home
         </button>
-      </div>
-      <div>
-        <Footer />
       </div>
     </>
   );
