388c641bc91e30a1f15781110b8ecc9e365aae5f
MatiasBarreto1595 <matias.barreto98@hotmail.com>
2023-12-11 11:36:21 -0300

    persist

--- src/redux/storeConfig.js
+++ src/redux/storeConfig.js
@@ -1,13 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
-import products from "./productsSlice";
-import cart from "./cartSlice";
-import buyer from "./buyerSlice";
-const store = configureStore({
-  reducer: {
-    products,
-    cart,
-    buyer,
-  },
-});
+import productsSlice from "./productsSlice";
+import cartSlice from "./cartSlice";
+import buyerSlice from "./buyerSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export default store;
+import { combineReducers } from "redux";
+const rootReducer = combineReducers({
+  products: productsSlice,
+  cart: cartSlice,
+  buyer: buyerSlice,
+});
+const persistConfig = { key: "root", storage };
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+const persistor = persistStore(store);
+
+export { persistor, store };
