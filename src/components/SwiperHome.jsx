0552dcd2f35aafaff6e92ed4927db64004c2071f
emzi16 <emilianozapata0638@gmail.com>
2023-12-14 14:38:44 -0300

    Update SwiperHome.jsx

--- src/components/SwiperHome.jsx
+++ src/components/SwiperHome.jsx
@@ -7,32 +7,6 @@ import SwiperStyle from "../styles/components/SwiperHome.module.css";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 function SwiperHome() {
-  //   const imgArr = [
-  //     {
-  //       id: 1,
-  //       original: "24_Juice_Shop_Andria_Lo.webp",
-  //       text: "Indulge in Pure Freshness! Vibrant juices that awaken your senses. Discover a burst of natural flavors. Order now and savor the goodness!",
-  //       class: "firstText",
-  //     },
-  //     {
-  //       id: 2,
-  //       original: "Green-Header-1.jpg",
-  //       text: `100% Organic · Bay area made · Family owned`,
-  //       class: "secondText",
-  //     },
-  //     {
-  //       id: 3,
-  //       original: "Tonic-2.webp",
-  //       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, dolorum.",
-  //       class: "thirdText",
-  //     },
-  //     {
-  //       id: 4,
-  //       original: "305849026_511483600978910_7244055434825137908_n.jpg",
-  //       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, dolorum.",
-  //       class: "fourthText",
-  //     },
-  //   ];
   const imgArr = [
     {
       id: 1,
@@ -43,25 +17,25 @@ function SwiperHome() {
     {
       id: 2,
       original: "11.webp",
-      text: "Indulge in Pure Freshness! Vibrant juices that awaken your senses. Discover a burst of natural flavors. Order now and savor the goodness!",
+      text: "Indulge in Pure Freshness! Vibrant juices that awaken your senses. Discover a burst of natural flavors.",
       class: "secondText",
     },
     {
       id: 3,
       original: "Tonic-2.jpg",
-      text: "Sparkle Up Your Day! Our Sparklin' Tonics - a fizz of pure delight. Elevate your moments with refreshing bliss. Try one today!",
+      text: "Sparkle Up Your Day! Our Sparklin' Tonics - a fizz of pure delight. Try one today!",
       class: "thirdText",
     },
     {
       id: 4,
       original: "12.png",
-      text: "Discover Our Global Oasis! From coast to coast, find our refreshing haven. Step into our worldwide locations and taste freshness worldwide!",
+      text: "Discover Our Global Oasis! From coast to coast, find our refreshing haven.",
       class: "fourthText",
     },
     {
       id: 5,
       original: "15.jpg",
-      text: "Streetwise Freshness! Our iconic street stands offer a taste of pure delight. Join the buzz and savor our renowned flavors on the go!",
+      text: "Streetwise Freshness! Our iconic street stands offer a taste of pure delight.",
       class: "fifthText",
     },
   ];
